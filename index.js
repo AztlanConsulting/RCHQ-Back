90905580fb81a82ded254e9a3a02924fd349f330
Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2026-03-20 17:12:46 -0600

    feat(api): Add Mailgun API Connection

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+
+// Loads the variables in the enviorment file
+require("dotenv").config();
+
+const port = Number(process.env.RUNNING_PORT);
 
 app.use(express.json());
 
-const userRouter = require('./router/user.route');
-app.use('/users', userRouter);
+const xd = require("./utils/mail");
+
+const userRouter = require("./router/user.route");
+app.use("/users", userRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
