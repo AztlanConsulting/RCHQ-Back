8c0c51f9c665f896c652c172f2d2bbb3908d421a
Manuel Bajos Rivera <A01711581@tec.mx>
2026-04-05 02:07:36 -0600

    add: prisma schema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const speakeasy = require("speakeasy");
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
 const app = express();
 
-const errorHandler = require("./middleware/ErrorHandler");
-
-// Loads the variables in the enviorment file
+// Loads the variables in the enviorment file before creating Prisma.
 require("dotenv").config();
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+const errorHandler = require("./middleware/ErrorHandler");
 
 const port = Number(process.env.RUNNING_PORT || 3000);
 
@@ -18,6 +23,19 @@ const userRouter = require("./router/user.route");
 app.use("/users", userRouter);
 
 app.use(errorHandler);
+
+app.get("/test", async (req, res) => {
+  const user = { texto: "Manuel" };
+  try {
+    const sent = await prisma.usuario.create({
+      data: user,
+    });
+    res.json(sent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error creating user" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
