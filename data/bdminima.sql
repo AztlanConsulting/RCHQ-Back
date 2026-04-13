cb9b543dc729726adb28c084296956022e5dad95
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-04-13 15:04:26 -0600

    Add: Limits were added to the table fields

--- data/bdminima.sql
+++ data/bdminima.sql
@@ -1,12 +1,12 @@
 CREATE TABLE public.donationtype (
 	donationtypeid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(30) NOT NULL,
 	CONSTRAINT donationtype_pk PRIMARY KEY (donationtypeid)
 );
 
 CREATE TABLE public.donator (
 	donatorid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(100) NOT NULL,
 	CONSTRAINT donator_pk PRIMARY KEY (donatorid)
 );
 
@@ -19,23 +19,23 @@ CREATE TABLE public.fault (
 
 CREATE TABLE public.workday (
 	workdayid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(9) NOT NULL,
 	CONSTRAINT workday_pk PRIMARY KEY (workdayid)
 );
 
 CREATE TABLE public.house (
 	houseid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(60) NOT NULL,
 	location text NOT NULL,
-	phonenumber varchar(10) NOT NULL,
+	phonenumber int(15) NOT NULL,
 	description text NOT NULL,
-	image varchar NOT NULL,
+	image varchar(100) NOT NULL,
 	CONSTRAINT house_pk PRIMARY KEY (houseid)
 );
 
 CREATE TABLE public.role (
 	roleid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(50) NOT NULL,
 	CONSTRAINT role_pk PRIMARY KEY (roleid)
 );
 
@@ -43,16 +43,16 @@ CREATE TABLE public.employee (
 	employeeid uuid NOT NULL,
 	houseid uuid NOT NULL,
 	roleid uuid NOT NULL,
-	name varchar NOT NULL,
-	surname varchar NOT NULL,
+	name varchar(50) NOT NULL,
+	surname varchar(50) NOT NULL,
 	isactive bool NULL,
-	email varchar NOT NULL,
+	email varchar(60) NOT NULL,
 	password varchar NOT NULL,
 	hasfirstlogin bool NOT NULL,
-	totpsecret varchar NULL,
-	curp varchar NULL,
+	totpsecret varchar(32) NULL,
+	curp varchar(18) NULL,
 	birthdate date NULL,
-	picture varchar NULL,
+	picture varchar(150) NULL,
 	startdate date NOT NULL,
 	nss varchar(11) NULL,
 	bank_account varchar(18) NULL,
@@ -66,9 +66,9 @@ CREATE TABLE public.donation (
 	donatorid uuid NOT NULL,
 	donationtypeid uuid NOT NULL,
 	date date NOT NULL,
-	product varchar NOT NULL,
+	product varchar(30) NOT NULL,
 	quantity decimal NOT NULL,
-	measurment varchar NOT NULL,
+	measurment varchar(10) NOT NULL,
 	CONSTRAINT donation_pk PRIMARY KEY (donationid),
 	CONSTRAINT donation_donator_fk FOREIGN KEY (donatorid) REFERENCES public.donator(donatorid),
 	CONSTRAINT donation_donationtype_fk FOREIGN KEY (donationtypeid) REFERENCES public.donationtype(donationtypeid)
@@ -84,8 +84,8 @@ CREATE TABLE public.employeefault (
 CREATE TABLE public.outsidecertification (
 	outsidecertificationid uuid NOT NULL,
 	employeeid uuid NOT NULL,
-	file varchar NOT NULL,
-	name varchar NOT NULL,
+	file varchar(100) NOT NULL,
+	name varchar(70) NOT NULL,
 	CONSTRAINT outsidecertification_pk PRIMARY KEY (outsidecertificationid),
 	CONSTRAINT outsidecertification_employee_fk FOREIGN KEY (employeeid) REFERENCES public.employee(employeeid)
 );
@@ -102,7 +102,7 @@ CREATE TABLE public.employeeworkday (
 CREATE TABLE public.psychologicalevaluation (
 	psychologicalevaluationid uuid NOT NULL,
 	employeeid uuid NOT NULL,
-	file varchar NOT NULL,
+	file varchar(100) NOT NULL,
 	date date NOT NULL,
 	CONSTRAINT psychologicalevaluation_pk PRIMARY KEY (psychologicalevaluationid)
 );
@@ -110,7 +110,7 @@ CREATE TABLE public.psychologicalevaluation (
 CREATE TABLE public.employeeaddress (
 	employeeaddressid uuid NOT NULL,
 	employeeid uuid NOT NULL,
-	url varchar NOT NULL,
+	url varchar(100) NOT NULL,
 	date timestamp NOT NULL,
 	CONSTRAINT employeeaddress_pk PRIMARY KEY (employeeaddressid),
 	CONSTRAINT employeeaddress_employee_fk FOREIGN KEY (employeeid) REFERENCES public.employee(employeeid)
@@ -131,7 +131,7 @@ CREATE TABLE public.logs (
 	logid uuid NOT NULL,
 	employeeid uuid NOT NULL,
 	moment timestamp NOT NULL,
-	description varchar NOT NULL,
+	description varchar(100) NOT NULL,
 	ip_address varchar(15) NOT NULL,
 	CONSTRAINT logs_pk PRIMARY KEY (logid),
 	CONSTRAINT logs_employee_fk FOREIGN KEY (employeeid) REFERENCES public.employee(employeeid)
@@ -140,14 +140,14 @@ CREATE TABLE public.logs (
 CREATE TABLE public.employeebackupcode (
 	backupcodeid uuid NOT NULL,
 	employeeid uuid NOT NULL,
-	code varchar NOT NULL,
+	code varchar(10) NOT NULL,
 	CONSTRAINT employeebackupcode_pk PRIMARY KEY (backupcodeid),
 	CONSTRAINT employeebackupcode_employee_fk FOREIGN KEY (employeeid) REFERENCES public.employee(employeeid)
 );
 
 CREATE TABLE public.insidecertifications (
 	insidecertificationid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(70) NOT NULL,
 	description text NOT NULL,
 	CONSTRAINT insidecertifications_pk PRIMARY KEY (insidecertificationid)
 );
@@ -162,38 +162,38 @@ CREATE TABLE public.employeeinsidecertification (
 
 CREATE TABLE public.documents (
 	documentid uuid NOT NULL,
-	cv varchar NULL,
-	birth_certificate varchar NULL,
-	tax_status_certificate varchar NULL,
-	address_certificate varchar NULL,
-	nss varchar NULL,
-	profesional_id varchar NULL,
-	education_certificate varchar NULL,
-	medical_certificate varchar NULL,
-	state_criminal_record_certificate varchar NULL,
-	federal_criminal_record_certificate varchar NULL,
-	first_recommendation_letter varchar NULL,
-	second_recommendation_letter varchar NULL,
-	driver_license varchar NULL,
-	signed_regulation varchar NULL,
-	signed_contract varchar NULL,
-	signed_confidential_letter varchar NULL,
-	signed_ethics_letter varchar NULL,
-	induction_manual varchar NULL,
+	cv varchar(100) NULL,
+	birth_certificate varchar(100) NULL,
+	tax_status_certificate varchar(100) NULL,
+	address_certificate varchar(100) NULL,
+	nss varchar(100) NULL,
+	profesional_id varchar(100) NULL,
+	education_certificate varchar(100) NULL,
+	medical_certificate varchar(100) NULL,
+	state_criminal_record_certificate varchar(100) NULL,
+	federal_criminal_record_certificate varchar(100) NULL,
+	first_recommendation_letter varchar(100) NULL,
+	second_recommendation_letter varchar(100) NULL,
+	driver_license varchar(100) NULL,
+	signed_regulation varchar(100) NULL,
+	signed_contract varchar(100) NULL,
+	signed_confidential_letter varchar(100) NULL,
+	signed_ethics_letter varchar(100) NULL,
+	induction_manual varchar(100) NULL,
 	CONSTRAINT documents_pk PRIMARY KEY (documentid)
 );
 
 CREATE TABLE public.employeedocuments (
 	documentid uuid NOT NULL,
 	employeeid uuid NOT NULL,
-	url varchar NOT NULL,
+	url varchar(100) NOT NULL,
 	CONSTRAINT employeedocuments_documents_fk FOREIGN KEY (documentid) REFERENCES public.documents(documentid),
 	CONSTRAINT employeedocuments_employee_fk FOREIGN KEY (employeeid) REFERENCES public.employee(employeeid)
 );
 
 CREATE TABLE public.eventtype (
 	eventtypeid uuid NOT NULL,
-	name varchar NOT NULL,
+	name varchar(30) NOT NULL,
 	CONSTRAINT eventtype_pk PRIMARY KEY (eventtypeid)
 );
 
@@ -213,7 +213,7 @@ CREATE TABLE public.personalevent (
 	eventtypeid uuid NOT NULL,
 	start timestamp NOT NULL,
 	end timestamp NOT NULL,
-	name varchar NOT NULL,
+	name varchar(70) NOT NULL,
 	description text NOT NULL,
 	CONSTRAINT personalevent_pk PRIMARY KEY (personaleventid),
 	CONSTRAINT personalevent_eventtype_fk FOREIGN KEY (eventtypeid) REFERENCES public.eventtype(eventtypeid)
@@ -226,7 +226,7 @@ CREATE TABLE public.houseevent (
 	date date NOT NULL,
 	start time NOT NULL,
 	end date NOT NULL,
-	name varchar NOT NULL,
+	name varchar(70) NOT NULL,
 	description text NULL,
 	CONSTRAINT houseevent_pk PRIMARY KEY (houseeventid),
 	CONSTRAINT houseevent_eventtype_fk FOREIGN KEY (eventtypeid) REFERENCES public.eventtype(eventtypeid),
@@ -239,7 +239,7 @@ CREATE TABLE public.globalevent (
 	date date NOT NULL,
 	start time NOT NULL,
 	end time NOT NULL,
-	name varchar NOT NULL,
+	name varchar(70) NOT NULL,
 	description text NULL,
 	isfreeday boolean NOT NULL,
 	CONSTRAINT globalevent_pk PRIMARY KEY (globaleventid),
