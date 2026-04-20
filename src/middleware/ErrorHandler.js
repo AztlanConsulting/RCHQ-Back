788a25128a37445888ffd531db31b88c9a72d60e
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-04-20 12:02:10 -0600

    fix: Se reduce el tamaño que recibe de las fotos de 25mb a 5mb

--- src/middleware/ErrorHandler.js
+++ src/middleware/ErrorHandler.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
  if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_SIZE") {
     return res.status(400).json({
-      error: "El archivo excede el tamaño permitido (25MB)",
+      error: "El archivo excede el tamaño permitido (5MB)",
     });
   }
 
--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage,
     limits: {
-        fileSize: 25 * 1024 * 1024,
+        fileSize: 5 * 1024 * 1024,
     },
     fileFilter: (req, file, cb) => {
         const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
