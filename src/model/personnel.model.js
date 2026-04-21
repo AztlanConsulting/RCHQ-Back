fb7ca581e1c19d271bc8276ffa0c280b2be90855
Andre Agle <andrezala03@gmail.com>
2026-04-21 16:00:31 -0600

    add: prisma queries for complete employee detail info

--- src/model/personnel.model.js
+++ src/model/personnel.model.js
@@ -69,15 +69,172 @@ async function getEmployeeById(employeeId) {
 // employee_address
 // employee_workday
 // employee_vacation_requests
-async function getAdminEmployeeInfoById() {
-    // const result = await prisma.employee
-    const result = "";
+async function getAdminEmployeeInfoById(employeeId) {
+  const employee = await prisma.employee.findUnique({
+    where: { employee_id: employeeId },
+    select: {
+      employee_fault: {
+        select: {
+          fault: {
+            select: {
+              fault_id: true,
+              date: true,
+              description: true,
+            },
+          },
+        },
+      },
+      employee_address: {
+        select: {
+          employee_address_id: true,
+          url: true,
+          date: true,
+        },
+        orderBy: { date: "desc" },
+      },
+      employee_workday: {
+        select: {
+          start: true,
+          end: true,
+          workday: {
+            select: {
+              workday_id: true,
+              name: true,
+            },
+          },
+        },
+      },
+      vacations_request: {
+        select: {
+          vacations_request_id: true,
+          start: true,
+          end: true,
+          status: true,
+          feedback: true,
+        },
+        orderBy: { start: "desc" },
+      },
+    },
+  });
 
-    return result;
+  if (!employee) return undefined;
+
+  return {
+    faults: employee.employee_fault.map((ef) => ({
+      faultId: ef.fault.fault_id,
+      date: ef.fault.date,
+      description: ef.fault.description,
+    })),
+    addresses: employee.employee_address.map((a) => ({
+      employeeAddressId: a.employee_address_id,
+      url: a.url,
+      date: a.date,
+    })),
+    workdays: employee.employee_workday.map((w) => ({
+      workdayId: w.workday.workday_id,
+      name: w.workday.name,
+      start: w.start,
+      end: w.end,
+    })),
+    vacationRequests: employee.vacations_request.map((v) => ({
+      vacationsRequestId: v.vacations_request_id,
+      start: v.start,
+      end: v.end,
+      status: v.status,
+      feedback: v.feedback,
+    })),
+  };
 }
 
-async function getEmployeeRecord() {
+// return urls and other metadata of the employee's record:
+// employee_documents -> collections of urls to important employee docs
+// employee_inside_certification
+// employee_outside_certification
+// employee_psychological_evaluation
+async function getEmployeeRecord(employeeId) {
+  const employee = await prisma.employee.findUnique({
+    where: { employee_id: employeeId },
+    select: {
+      employee_documents: {
+        select: {
+          url: true,
+          documents: true,
+        },
+      },
+      employee_inside_certification: {
+        select: {
+          date: true,
+          inside_certifications: {
+            select: {
+              inside_certification_id: true,
+              name: true,
+              description: true,
+            },
+          },
+        },
+      },
+      outside_certification: {
+        select: {
+          outside_certification_id: true,
+          file: true,
+          name: true,
+        },
+      },
+      psychological_evaluation: {
+        select: {
+          psychological_evaluation_id: true,
+          file: true,
+          date: true,
+        },
+        orderBy: { date: "desc" },
+      },
+    },
+  });
 
+  if (!employee) return undefined;
+
+  return {
+    documents: employee.employee_documents.map((d) => ({
+      documentId: d.documents.document_id,
+      url: d.url,
+      files: {
+        cv: d.documents.cv,
+        birthCertificate: d.documents.birth_certificate,
+        taxStatusCertificate: d.documents.tax_status_certificate,
+        addressCertificate: d.documents.address_certificate,
+        nss: d.documents.nss,
+        professionalId: d.documents.professional_id,
+        educationCertificate: d.documents.education_certificate,
+        medicalCertificate: d.documents.medical_certificate,
+        stateCriminalRecordCertificate: d.documents.state_criminal_record_certificate,
+        federalCriminalRecordCertificate: d.documents.federal_criminal_record_certificate,
+        firstRecommendationLetter: d.documents.first_recommendation_letter,
+        secondRecommendationLetter: d.documents.second_recommendation_letter,
+        driverLicense: d.documents.driver_license,
+        signedRegulation: d.documents.signed_regulation,
+        signedContract: d.documents.signed_contract,
+        signedConfidentialLetter: d.documents.signed_confidential_letter,
+        signedEthicsLetter: d.documents.signed_ethics_letter,
+        inductionManual: d.documents.induction_manual,
+      },
+    })),
+    insideCertifications: employee.employee_inside_certification.map((c) => ({
+      insideCertificationId: c.inside_certifications.inside_certification_id,
+      name: c.inside_certifications.name,
+      description: c.inside_certifications.description,
+      date: c.date,
+    })),
+    outsideCertifications: employee.outside_certification.map((c) => ({
+      outsideCertificationId: c.outside_certification_id,
+      name: c.name,
+      file: c.file,
+    })),
+    psychologicalEvaluations: employee.psychological_evaluation.map((p) => ({
+      psychologicalEvaluationId: p.psychological_evaluation_id,
+      file: p.file,
+      date: p.date,
+    })),
+  };
 }
 
 module.exports = {
