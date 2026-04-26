6ed787e33c778e101870966b6c9d90038d1574ef
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-04-26 15:51:28 -0600

    feat: Se añade paginacion al momento de obtener a los empleados

--- src/model/employee/getAll.model.js
+++ src/model/employee/getAll.model.js
@@ -1,36 +1,50 @@
 const prisma = require("../../prisma");
 
-const getEmployees = async (houseId, active) => {
-    const employees = await prisma.employee.findMany({
-        where: {
-            house_id: houseId,
-            is_active: active,
-        },
-        select: {
-            employee_id: true,
-            name: true,
-            surname: true,
-            picture: true,
-            is_active: true,
-            role: {
-                select: {
-                    name: true,
+const getEmployees = async (houseId, active, skip, take) => {
+    const [employees, total] = await Promise.all([
+        prisma.employee.findMany({
+            where: {
+                house_id: houseId,
+                is_active: active,
+            },
+            select: {
+                employee_id: true,
+                name: true,
+                surname: true,
+                picture: true,
+                is_active: true,
+                role: {
+                    select: {
+                        name: true,
+                    },
                 },
             },
-        },
-        orderBy: {
-            name: "asc",
-        },
-    });
+            orderBy: {
+                name: "asc",
+            },
+            skip,
+            take,
+        }),
 
-    return employees.map((employee) => ({
-        employeeId: employee.employee_id,
-        name: employee.name,
-        surname: employee.surname,
-        picture: employee.picture,
-        isActive: employee.is_active,
-        roleName: employee.role.name,
-    }));
+        prisma.employee.count({
+            where: {
+                house_id: houseId,
+                is_active: active,
+            },
+        }),
+    ]);
+
+    return {
+        employees: employees.map((employee) => ({
+            employeeId: employee.employee_id,
+            name: employee.name,
+            surname: employee.surname,
+            picture: employee.picture,
+            isActive: employee.is_active,
+            roleName: employee.role.name,
+        })),
+        total,
+    };
 };
 
 module.exports = {
