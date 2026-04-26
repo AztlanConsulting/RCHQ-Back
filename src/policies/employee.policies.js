ab08f2cb1f4eff9a489a2d2e84bd0ed4328a7892
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-04-26 15:36:32 -0600

    refactor: Se refactoriza el archivo de employeePolocies para cubrir todas las HU relacionadas a employee

---
+++ src/policies/employee.policies.js
@@ -0,0 +1,13 @@
+const employeePolicy = (user) => {
+    if (!user) return false;
+
+    if (user.role === "Administrador") return true;
+
+    if (user.role === "Coordinador") return true;
+
+    return false;
+};
+
+module.exports = {
+    employeePolicy,
+};
