18b9d799f7a5f62e6ce24b51afbb6938f920acda
Manu090704 <A01711581@tec.mx>
2026-04-27 02:27:55 -0600

    fix: transformIgnorePatterns movido a jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
 module.exports = {
   testEnvironment: "node",
-
   verbose: true,
-
   collectCoverage: true,
   coverageDirectory: "coverage",
-
   testMatch: [
     "<rootDir>/src/tests/unit/**/*.test.js",
     "<rootDir>/src/tests/integration/**/*.test.js",
   ],
-
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testTimeout: 15000,
+  transformIgnorePatterns: [
+    "/node_modules/(?!(uuid)/)"
+  ],
 };
