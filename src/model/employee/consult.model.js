5778a4df5279f6a374236f6ebc3ab591ab6f73a8
Ivan <upp.estancia@gmail.com>
2026-04-29 09:05:47 -0600

    fix: ajusta consult model de employee con comentario pendiente

--- src/model/employee/consult.model.js
+++ src/model/employee/consult.model.js
@@ -6,17 +6,19 @@ exports.findByCurp = async (curp) => {
     });
 }
 
-async function findById(employee_id) {
-    return await prisma.employee.findUnique({
-        where: { employee_id: employee_id },
-    });
-}
+// Estas funciones están declaradas dos veces, comenté estas para
+// usar las segundas, pero falta aclarar cuales son las correctas
+// async function findById(employee_id) {
+//     return await prisma.employee.findUnique({
+//         where: { employee_id: employee_id },
+//     });
+// }
 
-async function getAllRoles() {
-    return await prisma.role.findMany();
-}
+// async function getAllRoles() {
+//     return await prisma.role.findMany();
+// }
 
-async function getWorkDays(employeeId) {
+exports.getWorkDays = async (employeeId) => {
     return await prisma.employee_workday.findMany({
         where: {
             employee_id: employeeId
@@ -27,7 +29,7 @@ async function getWorkDays(employeeId) {
     });
 }
 
-async function getHome(employeeId) {
+exports.getHome = async (employeeId) => {
     return await prisma.employee.findUnique({
         where: {
             employee_id: employeeId
@@ -38,7 +40,7 @@ async function getHome(employeeId) {
     });
 }
 
-async function getStartDate(employeeId) {
+exports.getStartDate = async (employeeId) => {
     return await prisma.employee.findUnique({
         where: {
             employee_id: employeeId
@@ -49,9 +51,9 @@ async function getStartDate(employeeId) {
     });
 }
 
-exports.findById = async (employee_id) => {
+exports.findById = async (employeeId) => {
     return await prisma.employee.findUnique({
-        where: { employee_id },
+        where: { employee_id: employeeId },
     });
 };
 
@@ -75,11 +77,3 @@ exports.findByIdWithRoleAndHouse = async (employeeId) => {
         },
     });
 }
-
-module.exports = {
-    findById,
-    getAllRoles,
-    getWorkDays,
-    getHome,
-    getStartDate
-};
