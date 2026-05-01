dcea9ff7296f140d11c37c12996cfa6c9b2ec004
Andre Agle <andrezala03@gmail.com>
2026-04-30 21:05:32 -0600

    fix: quitar archivos sql no usados

--- data/rchq_test_01_create_database.sql
+++
@@ -1,7 +0,0 @@
--- DBeaver: conéctate a la base "postgres" (o template1) y ejecuta SOLO el siguiente bloque.
--- No pegues aquí el script de tablas: PostgreSQL no usa "USE db"; se elige la BD en el cliente
--- o con \c "RCHQ-test" en psql.
---
--- Después, abre conexión a "RCHQ-test" (o pásala en la URL) y ejecuta: rchq_test_02_from_prisma_migrations.sql
-
-CREATE DATABASE "RCHQ-test";
