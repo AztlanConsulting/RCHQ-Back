8c2b6ca5b6d2c85c85804e54da590f6593b4f5d6
Manu090704 <A01711581@tec.mx>
2026-05-02 03:21:24 -0600

    fix: (employee.create.test) se modifico a Admin en lugar de Administrador

--- src/tests/unit/employee.create.test.js
+++ src/tests/unit/employee.create.test.js
@@ -25,7 +25,7 @@ const { createEmployee } = require("../../service/employee/create.service");
 describe("Employee Service - createEmployee", () => {
   const mockUserAdmin = {
     id: "user-1",
-    role: "Administrador",
+    role: "Admin",
     houseId: "a0000001-0000-4000-8000-000000000001",
   };
 
