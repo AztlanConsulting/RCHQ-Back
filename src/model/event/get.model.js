21b567a4e0e8784ec8ab3be16926291ec266237f
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-05-13 19:10:59 -0600

    refactor(eventModel): Se refactoriza el modelo para atender los cambios de la db

--- src/model/event/get.model.js
+++ src/model/event/get.model.js
@@ -1,62 +1,63 @@
 const prisma = require("../../prisma");
 
 exports.getAllEventTypes = async () => {
-    const result = await prisma.event_type.findMany({
+    return await prisma.event_type.findMany({
         select: {
             name: true,
-        }
+        },
     });
-    return result;
-}
+};
 
 exports.getHouseEventsInRange = async (houseId, startDate, endDate) => {
     return await prisma.house_event.findMany({
         where: {
             house_id: houseId,
-            date: {
+            start: {
+                lte: endDate,
+            },
+            end: {
                 gte: startDate,
-                lte: endDate
             },
         },
-        include : {
-            event_type: true
-        }
+        include: {
+            event_type: true,
+        },
     });
-}
+};
 
 exports.getPersonalEventsInRange = async (employeeId, startDate, endDate) => {
     return await prisma.employee_personal_event.findMany({
         where: {
             employee_id: employeeId,
             personal_event: {
-                start: {
-                    lte: endDate
+                date: {
+                    gte: startDate,
+                    lte: endDate,
                 },
-                end: {
-                    gte: startDate
-                }
-            }
+            },
         },
-        include : {
+        include: {
             personal_event: {
                 include: {
-                    event_type: true
-                }
-            }
-        }
+                    event_type: true,
+                },
+            },
+        },
     });
-}
+};
 
 exports.getGlobalEventsInRange = async (startDate, endDate) => {
     return await prisma.global_event.findMany({
         where: {
-            date: {
+            start: {
+                lte: endDate,
+            },
+            end: {
                 gte: startDate,
-                lte: endDate
             },
         },
-        include : {
-            event_type: true
-        }
+        include: {
+            event_type: true,
+        },
     });
-}
+};
