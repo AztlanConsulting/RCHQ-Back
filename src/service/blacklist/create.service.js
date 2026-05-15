7ba38c5ce0440dda1f3fa132edc4fb655d5262dc
Oswaldo Hernández <a01199004@tec.mx>
2026-05-14 22:48:43 -0600

    fix: se sigue el estándar definido por el equipo donde el service manda siempre CODE de respuesta en vez de tener un status directo

--- src/service/blacklist/create.service.js
+++ src/service/blacklist/create.service.js
@@ -5,34 +5,39 @@ const { LOG_ACTIONS } = require("../../utils/logActions");
 const RESPONSES = require("../../utils/responses");
 
 exports.insertIntoBlacklist = async (curp, executorId, ipAddress) => {
-    const employee = await findEmployeeByCurp(curp);
-    if (!employee) return { code: RESPONSES.BLACKLIST.EMPLOYEE_NOT_FOUND };
-
-    if (employee.isBlacklisted) return { code: RESPONSES.BLACKLIST.ALREADY_EXISTS };
-
-    const blacklistEntry = await transactionalBlacklistInsert(curp);
-    if (!blacklistEntry) return { code: RESPONSES.BLACKLIST.INSERT_FAILED };
-
-    let warning = null;
     try {
-        await createLog(
-            executorId,
-            LOG_ACTIONS.BLACKLIST_ADDED,
-            ipAddress,
-            `${employee.name} ${employee.surname} - ${employee.curp}`
-        );
-    } catch (err) {
-        console.error("Error creando log insertIntoBlacklist:", err);
-        warning = "Empleado agregado a la lista negra, pero falló el registro de auditoría (log).";
-    }
+        const employee = await findEmployeeByCurp(curp);
+        if (!employee) return { code: RESPONSES.BLACKLIST.EMPLOYEE_NOT_FOUND };
 
-    return {
-        code: RESPONSES.BLACKLIST.ADDED,
-        data: {
-            employeeFullName: `${employee.name} ${employee.surname}`,
-            curp: employee.curp,
-            blacklistEntry,
-            warning,
-        },
-    };
+        if (employee.isBlacklisted) return { code: RESPONSES.BLACKLIST.ALREADY_EXISTS };
+
+        const blacklistEntry = await transactionalBlacklistInsert(curp);
+        if (!blacklistEntry) return { code: RESPONSES.BLACKLIST.INSERT_FAILED };
+
+        let warning = null;
+        try {
+            await createLog(
+                executorId,
+                LOG_ACTIONS.BLACKLIST_ADDED,
+                ipAddress,
+                `${employee.name} ${employee.surname} - ${employee.curp}`
+            );
+        } catch (err) {
+            console.error("Error creando log insertIntoBlacklist:", err);
+            warning = "Empleado agregado a la lista negra, pero falló el registro de auditoría (log).";
+        }
+
+        return {
+            code: RESPONSES.BLACKLIST.ADDED,
+            data: {
+                employeeFullName: `${employee.name} ${employee.surname}`,
+                curp: employee.curp,
+                blacklistEntry,
+                warning,
+            },
+        };
+    } catch (error) {
+        console.error("Error inesperado en insertIntoBlacklist service:", error);
+        return { code: RESPONSES.BLACKLIST.INTERNAL_ERROR };
+    }
 };
