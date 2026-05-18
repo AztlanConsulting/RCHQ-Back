d38a8da62553fe29fd63db5db7fb4123d2d44e8e
Andre Agle <andrezala03@gmail.com>
2026-05-18 04:16:46 -0600

    fix: convertir fechas UTC en eventos a tiempo MX antes de pasar al front

--- src/service/event/get.service.js
+++ src/service/event/get.service.js
@@ -8,6 +8,7 @@ const {
     calculateUsedDays,
     combineDateAndTime,
     stringToDate,
+    convertUTCToMexicanTime,
 } = require("../../utils/dates");
 const {
     getHome,
@@ -93,8 +94,8 @@ exports.getEventsInRange = async (employeeId, rawStartDate, rawEndDate) => {
         );
         houseEvents.forEach((event) => {
             events.push({
-                start: event.start,
-                end: event.end,
+                start: convertUTCToMexicanTime(event.start),
+                end: convertUTCToMexicanTime(event.end),
                 date: "",
                 name: event.name,
                 type: event.event_type.name,
@@ -141,8 +142,8 @@ exports.getEventsInRange = async (employeeId, rawStartDate, rawEndDate) => {
     const globalEvents = await getGlobalEventsInRange(startDate, endDate);
     globalEvents.forEach((event) => {
         events.push({
-            start: event.start,
-            end: event.end,
+            start: convertUTCToMexicanTime(event.start),
+            end: convertUTCToMexicanTime(event.end),
             date: "",
             name: event.name,
             subtitle: event.subtitle || "",
