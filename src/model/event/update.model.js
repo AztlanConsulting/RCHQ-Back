1ca8a7945e4f647dff9e7473eabc6dadb7630e20
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-05-20 18:10:11 -0600

    fix(delete): Se cambian funciones de get a el archivo correspondiente.

--- src/model/event/update.model.js
+++ src/model/event/update.model.js
@@ -1,39 +1,6 @@
 const prisma = require("../../prisma");
 const { mapHouseEvent } = require("../../utils/mappers/event.map");
 
-exports.findHouseEventById = async (eventId, houseId) => {
-    return await prisma.house_event.findFirst({
-        where: {
-            house_event_id: eventId,
-            house_id: houseId,
-        },
-    });
-};
-
-exports.findOverlappingHouseEvents = async ({
-    houseId,
-    start,
-    end,
-    excludeEventId,
-}) => {
-    const houseEvents = await prisma.house_event.findMany({
-        where: {
-            house_id: houseId,
-            house_event_id: { not: excludeEventId },
-            start: { lt: end },
-            end: { gt: start },
-        },
-        select: {
-            house_event_id: true,
-            name: true,
-            start: true,
-            end: true,
-        },
-    });
-
-    return houseEvents.map(mapHouseEvent);
-};
-
 exports.updateHouseEvent = async (eventId, data) => {
     const houseEvent = await prisma.house_event.update({
         where: { house_event_id: eventId },
