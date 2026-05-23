2d91920266abeed0554ac55ba9c4058a048285c3
Oswaldo Hernández <a01199004@tec.mx>
2026-05-22 19:20:10 -0600

    fix: se agrega el campo faltante de la bd en los archivos de la US

--- src/model/blacklist/get.model.js
+++ src/model/blacklist/get.model.js
@@ -29,3 +29,74 @@ exports.findEmployeeByCurp = async (curp) => {
         return null;
     }
 };
+
+exports.getBlacklistedEmployees = async ({ page, limit, curp, isBlacklisted }) => {
+    try {
+        const skip = (page - 1) * limit;
+
+        const whereClause = {};
+        if (curp) {
+            whereClause.curp = { contains: curp, mode: "insensitive" };
+        }
+        if (typeof isBlacklisted === "boolean") {
+            whereClause.blacklist = isBlacklisted ? { isNot: null } : null;
+        }
+
+        const totalItems = await prisma.employee.count({ where: whereClause });
+
+        const employees = await prisma.employee.findMany({
+            where: whereClause,
+            skip: skip,
+            take: limit,
+            select: {
+                picture: true,
+                name: true,
+                surname: true,
+                rfc: true,
+                curp: true,
+                nss: true,
+                is_active: true,
+                house: {
+                    select: {
+                        name: true,
+                    },
+                },
+                role: {
+                    select: {
+                        name: true,
+                    },
+                },
+                blacklist: {
+                    select: { blacklist_id: true }
+                }
+            },
+            orderBy: {
+                name: "asc",
+            },
+        });
+
+        const formattedEmployees = employees.map((emp) => ({
+            picture: emp.picture,
+            fullName: `${emp.name} ${emp.surname}`,
+            rfc: emp.rfc,
+            curp: emp.curp,
+            houseName: emp.house.name,
+            roleName: emp.role.name,
+            nss: emp.nss,
+            status: emp.is_active ? "Activo" : "Inactivo",
+            isBlacklisted: !!emp.blacklist,
+        }));
+
+        return {
+            employees: formattedEmployees,
+            pagination: {
+                totalItems,
+                totalPages: Math.ceil(totalItems / limit),
+                currentPage: page,
+            },
+        };
+    } catch (error) {
+        console.error("Error en getBlacklistedEmployees:", error);
+        return null;
+    }
+};
