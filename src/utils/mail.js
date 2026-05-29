acf3d362b92e72034e549a00a1342ef231bca45a
Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2026-05-29 01:39:02 -0600

    feat: modifica env para aceptar correos

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,4 @@
-/**
+
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
@@ -18,4 +18,3 @@ exports.send = async (destinatary, subject, message) => {
     .then((msg) => console.log(msg)) // logs response data
     .catch((err) => console.error(err)); // logs any error
 };
- */
