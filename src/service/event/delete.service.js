0e833394fc8576dfb4834b982678bb81a5e519ef
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-06-02 17:14:55 -0600

    feat(service): Se crea el servicio para eliminar un empelado de una capacitación y crear el log de la accion.

--- src/service/event/delete.service.js
+++ src/service/event/delete.service.js
@@ -1,10 +1,13 @@
 const {
     softDeleteHouseEvent,
     softDeletePersonalEvent,
+    removeEmployeeFromPersonalEvent,
 } = require("../../model/event/delete.model");
 const {
     findHouseEventByIdAndHouseId,
     findPersonalEventById,
+    findPersonalEventByIdIncludeDeleted,
+    findEmployeeInPersonalEvent,
 } = require("../../model/event/get.model");
 const { createLog } = require("../../model/log.model");
 const { LOG_ACTIONS } = require("../../utils/logActions");
@@ -67,4 +70,42 @@ exports.deletePersonalEvent = async (personalEventId, user, clientIp) => {
     }
 
     return { code: RESPONSES.EVENTS.DELETED };
+};
+
+exports.removeEmployeeFromPersonalEvent = async (personalEventId, employeeId, user, clientIp) => {
+    const event = await findPersonalEventByIdIncludeDeleted(personalEventId, user.houseId);
+
+    if (!event) {
+        return { code: RESPONSES.EVENTS.NOT_FOUND };
+    }
+
+    if (event.event_type?.name !== "Capacitaciones") {
+        return { code: RESPONSES.EVENTS.NOT_TRAINING_EVENT };
+    }
+
+    const relation = await findEmployeeInPersonalEvent(personalEventId, employeeId);
+
+    if (!relation) {
+        return { code: RESPONSES.EVENTS.EMPLOYEE_NOT_IN_EVENT };
+    }
+
+    try {
+        await removeEmployeeFromPersonalEvent(personalEventId, employeeId);
+    } catch (error) {
+        console.error("Error eliminando empleado del evento de personal:", error);
+        throw error;
+    }
+
+    try {
+        await createLog(
+            user.id,
+            LOG_ACTIONS.PERSONAL_EVENT_EMPLOYEE_REMOVED,
+            clientIp,
+            personalEventId,
+        );
+    } catch (error) {
+        console.error("Error creando log de eliminación de empleado de evento:", error);
+    }
+
+    return { code: RESPONSES.EVENTS.EMPLOYEE_REMOVED };
 };
